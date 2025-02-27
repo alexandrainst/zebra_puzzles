89d3630c03f1df8567336ea16d4d491888392c65
Sofie Helene Bruun <sofie.bruun@alexandra.dk>
2025-02-27 09:53:14 +0100

    Draft complete_clue()

--- src/zebra_puzzles/zebra_utils.py
+++ src/zebra_puzzles/zebra_utils.py
@@ -33,7 +33,41 @@ def define_clues(clues_included: str) -> List:
         ]
     else:
         raise ValueError("Unsupported clues '{clues_included}'")
+
     return clues
+
+
+def complete_clue(
+    clue: str,
+    n_objects: int,
+    attributes: Dict[str, Dict[str, str]],
+    chosen_attributes: List[List],
+) -> str:
+    """Complete the chosen clue type with random parts of the solution to create a full clue.
+
+    TODO: Consider how the clues will be evaluted. We should probably save more than a string.
+    TODO: Move the clue descriptions to the config file.
+
+    Args:
+        clue: Chosen clue type as a string.
+        n_objects: Number of objects in the puzzle as an int.
+        attributes: Possible attributes as a dictionary of dictionaries.
+        chosen_attributes: Attribute values chosen for the solution as a list of lists.
+
+    Returns:
+        full_clue: Full clue as a string.
+    """
+    if clue == "found_at":
+        i_object = sample(range(n_objects), 1)[0]
+        attribute = sample(chosen_attributes[i_object], 1)[0]
+        attribute_desc = attributes[attribute]
+        full_clue = "Personen der {attribute_desc} er ved hus nummer {i}.".format(
+            attribute_desc=attribute_desc, i=i_object
+        )
+    else:
+        raise ValueError("Unsupported clue '{clue}'")
+
+    return full_clue
 
 
 def generate_solution(
@@ -47,7 +81,7 @@ def generate_solution(
         n_attributes: Number of attributes of each object.
 
     Returns:
-        solution: A solution to a zebra puzzle as a list of lists representing the matrix of object indices and chosen attributes. This matrix is n_attributes x n_objects.
+        solution: A solution to a zebra puzzle as a list of lists representing the matrix of object indices and chosen attributes. This matrix is n_objects x n_attributes.
         chosen_categories: Categories chosen for the solution.
         chosen_attributes: Attribute values chosen for the solution.
     """
@@ -59,9 +93,12 @@ def generate_solution(
         sample(list(attributes[cat].keys()), k=n_objects) for cat in chosen_categories
     ]
 
-    # Add the object indices to the solution
-    indices = [str(x) for x in list(range(n_objects))]
-    solution = [indices] + chosen_attributes
+    # Transpose the attribute matrix
+    chosen_attributes = [
+        [row[i] for row in chosen_attributes] for i in range(n_attributes)
+    ]
+
+    solution = [[str(i)] + row for i, row in enumerate(chosen_attributes)]
 
     return solution, chosen_categories, chosen_attributes
 
@@ -75,7 +112,7 @@ def choose_clues(
     """Generate a zebra puzzle.
 
     Args:
-        solution: Solution to the zebra puzzle as a list of lists representing the solution matrix of object indices and chosen attribute values. This matrix is n_attributes x n_objects.
+        solution: Solution to the zebra puzzle as a list of lists representing the solution matrix of object indices and chosen attribute values. This matrix is n_objects x n_attributes.
         clues: Possible clues to include in the clues as a list of tuples. Each tuple contains the clue name and function. TODO: Edit this description when the clues are implemented.
         chosen_categories: Categories chosen for the solution.
         chosen_attributes: Attribute values chosen for the solution.
@@ -85,7 +122,5 @@ def choose_clues(
 
     TODO: Implement the generation of the clues.
     """
-    chosen_clues = (
-        "1. This is an example. \n 2. This is the second part of the example."
-    )
+    chosen_clues = "1. This is an example. \n2. This is the second part of the example."
     return chosen_clues
