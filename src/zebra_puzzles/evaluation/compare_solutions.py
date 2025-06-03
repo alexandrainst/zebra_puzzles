351fb00e05ccce288dc3b3a9973332bb50202fd9
Sofie Helene Bruun <sofie.bruun@alexandra.dk>
2025-06-03 17:14:49 +0200

    Delete renamed module and add more __init__.py files

--- src/zebra_puzzles/evaluation/compare_solutions.py
+++ src/zebra_puzzles/evaluation/compare_solutions.py
@@ -6,7 +6,7 @@ import logging
 import numpy as np
 from pydantic import BaseModel
 
-from zebra_puzzles.zebra_utils import bernoulli_std, round_using_std
+from zebra_puzzles.zebra_utils import bernoulli_std, capitalize, round_using_std
 
 log = logging.getLogger(__name__)
 
@@ -278,7 +278,7 @@ def format_scores(
     # Complete the string describing all metrics
     metrics_str = ""
     for score_type in score_types:
-        metrics_str += f"{score_type.capitalize()}:\n"
+        metrics_str += f"{capitalize(score_type)}:\n"
         metrics_str += metrics[score_type][-1]
         metrics_str += "\n\n"
 
