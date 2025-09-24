7b64c602f4d3a340baff52633ab6c755641cf70d
Sofie Helene Bruun <sofie.bruun@alexandra.dk>
2025-09-24 11:30:46 +0000

    Fix string formatting

--- src/scripts/format_datasets.py
+++ src/scripts/format_datasets.py
@@ -111,7 +111,7 @@ def format_datasets_pipeline(
         # Save datasets
         split_dataset.save_to_disk(Path(data_folder_current) / dataset_name)
         log.info(
-            f"Dataset {dataset_name} formatted and saved to {data_folder_current}/."
+            f"Dataset {dataset_name} formatted and saved to {data_folder_current}."
         )
 
     # Ask user if they want to push the dataset to Hugging Face Hub
